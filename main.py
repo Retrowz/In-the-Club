1ed46b46b65b8f09c329f92e2f615b6a4bb71855
Retrowz <>
2022-02-10 20:26:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Greet the user 
+print("Hello, Welcome to the Club!")
+#Ask the use their name
+str_name = input("What is your name? ")
+#Repeat their name back
+print("Wassup " + str_name + "!")
+#Ask how old they are
+print("How old are you" + "?")
+#if over 18 accept
+#if under stop
+      
